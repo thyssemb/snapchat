10fcebe51e1727298c8276073781c6fb7b320175
thyssem <thyssem.bazizi-makhlouf@epitech.eu>
2024-05-06 16:34:29 +0200

    [FIX] fix bug, center icon

--- Snapchat/App.js
+++ Snapchat/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Welcome from "./app/components/buttons/welcome";
+import { StyleSheet, View, Image } from 'react-native';
 import Register from "./app/components/buttons/register";
 import Login from "./app/components/buttons/login";
-function App() {
 
-  return (
-      <View style={styles.container}>
-          <Register />
-          <Login />
-      </View>
-  );
+function App() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.center}>
+                <Image
+                    source={require('./assets/snapchat-icon.png')}
+                    style={styles.image}
+                />
+            </View>
+            <View style={styles.bottom}>
+                <Register />
+                <Login />
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 6,
-    backgroundColor: '#fffc1f',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fffc1f',
+    },
+    center: {
+        flex: 4,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    bottom: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    image: {
+        width: 200,
+        height: 200,
+    },
 });
-export default App
+
+export default App;
